17332d2de71e0d6134188ed14d5efc5aef3652fd
Liman <liman@example.com>
2026-03-28 19:54:03 +0000

    feat: 更新 ResumeModule - 导入 CosModule

--- backend/src/modules/resume/resume.module.ts
+++ backend/src/modules/resume/resume.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CosModule } from '../../common/storage/cos.module';
 import { ResumeController } from './resume.controller';
 import { ResumeService } from './resume.service';
 import { Resume } from './resume.entity';
 import { ResumeFolder } from './resume-folder.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resume, ResumeFolder])],
+  imports: [TypeOrmModule.forFeature([Resume, ResumeFolder]), CosModule],
   controllers: [ResumeController],
   providers: [ResumeService],
   exports: [ResumeService],
